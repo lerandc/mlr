49a40a812c1aae22a962070d09f18f944f1857e5
lerandc <luisrd@berkeley.edu>
2024-06-10 14:46:20 -0700

    folder generation for parallel processes

--- mlr/database/utils.py
+++ mlr/database/utils.py
@@ -18,6 +18,27 @@ def gen_folder_name(base="particle"):
     folderName = base + "_" + time + fTime+ formatToday
     return folderName
 
+def gen_folder_name_by_thread(rank, N_processes, base="particle"):
+    today = datetime.today()
+    formatToday = today.strftime("_%Y%m%d")
+    hTime = int(today.strftime("%H"))
+    mTime = int(today.strftime("%M"))
+    sTime = int(today.strftime("%S"))
+
+    fTime = today.microsecond
+    
+    # separate into blocks of valid microseconds
+    # ensures no collision between processes of different ranks
+    my_fTime = fTime - (fTime % N_processes) + rank 
+
+    #convert time into seconds
+    time = (hTime*3600) + (mTime*60) + sTime 
+    time = str(time)
+
+    #add together for file name
+    folderName = base + "_" + time + str(my_fTime) + formatToday
+    return folderName
+
 def get_new_folder(mkdir=True, base="particle"):
     new_folder = gen_folder_name(base)
     while(pathlib.Path(new_folder).is_dir()):
@@ -25,6 +46,23 @@ def get_new_folder(mkdir=True, base="particle"):
 
     if mkdir:
         pathlib.Path(new_folder).mkdir(parents=True)
+
+    return pathlib.Path(new_folder)
+
+def get_new_folder_parallel(rank, N_processes, mkdir=True, base="particle"):
+    new_folder = gen_folder_name_by_thread(rank, N_processes, base)
+
+    successful = False
+    while(not successful):
+        while(pathlib.Path(new_folder).is_dir()):
+            new_folder = gen_folder_name_by_thread(rank, N_processes, base)
+
+        try:
+            if mkdir: #TODO when are we not making the directory?
+                pathlib.Path(new_folder).mkdir(parents=True)
+            successful = True
+        except FileExistsError:
+            successful = False
 
     return pathlib.Path(new_folder)
 
