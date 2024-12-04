f60a5fa1ac44dc87595be50a6faf9c467727e44a
lerandc <luisrd@berkeley.edu>
2024-12-04 10:29:26 -0800

    add utility class for forcing explicit specification and validation of class arguments, e.g., for neural network architecture

--- mlr/utils/configs.py
+++ mlr/utils/configs.py
@@ -16,17 +16,20 @@ def required_args(sig, filter_self=True):
         else:
             return False
 
-    args = {k: v for k, v in sig.parameters.items() if check_param(v)}
+    args = {k: v.annotation for k, v in sig.parameters.items() if check_param(v)}
     if filter_self and "self" in args:
         del args["self"]
 
-    return args.keys()
+    return args
 
 
 class Config(Mapping):
     def __init__(self, mclass, *args, **kwargs):
         self.metadata_class = mclass
-        self.metadata = self.metadata_class(*args, **kwargs)
+        try:
+            self.metadata = self.metadata_class(*args, **kwargs)
+        except TypeError:
+            raise TypeError(f"{self.metadata_class.__name__} requires the following arguments: {self.metadata_class.__annotations__}")
 
     @property
     def metadata(self):
@@ -34,7 +37,6 @@ class Config(Mapping):
 
     @metadata.setter
     def metadata(self, mdata):
-        ## Test for interface compatible with dataclass
         try:
             kv = {k: getattr(mdata, k) for k in get_annotations(type(mdata))}
             if len(kv) == 0:
@@ -58,34 +60,36 @@ class Config(Mapping):
 
 
 class FConfig:
-    def __init__(self, conf_class, **kwargs):
+    def __init__(self, conf_class, **added_args):
         # Store the class associated with the config
         if isclass(conf_class):
             self.conf_class = conf_class
         else:
             self.conf_class = type(conf_class)
 
-        init_sig = signature(getattr(self.conf_class, "__init__"))
+        self.init_sig = signature(getattr(self.conf_class, "__init__"))
+        self.def_args = required_args(self.init_sig)
+        self.added_args = added_args
+        self.req_args =  {**self.def_args, **self.added_args}
 
         self.metadata_class = make_dataclass(
             self.conf_class.__name__ + "_Config",
-            fields=list(required_args(init_sig)) + [(k, v) for k, v in kwargs.items()],
+            fields=[(k, v) for k, v in self.req_args.items()],
         )
+
         match self._validate_config():
             case True:
                 pass
             case False, missing_args:
                 raise ValueError(
-                    f"Specified configuration {kwargs}"
+                    f"Specified configuration {added_args}"
                     f" is missing required arguments {missing_args}"
                     f" of __init__ for {self.metadata_class.__name__}"
                 )
 
     def _validate_config(self):
-        init_sig = signature(getattr(self.conf_class, "__init__"))
-        req_args = set(required_args(init_sig))
         test_args = set(get_annotations(self.metadata_class).keys())
-        missing_args = req_args.difference(test_args)
+        missing_args = set(self.req_args.keys()).difference(test_args)
         if len(missing_args) > 0:
             return False, missing_args
         else:
@@ -93,3 +97,6 @@ class FConfig:
 
     def __call__(self, *args, **kwargs):
         return Config(self.metadata_class, *args, **kwargs)
+    
+    def __repr__(self):
+        return f"FConfig(conf_class={self.conf_class.__name__}, {self.added_args})"
